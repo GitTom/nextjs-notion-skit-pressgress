6ddce20f3173dc511a5e3a3b9d1755cb0fd426c2
Tom <GitTom@users.noreply.github.com>
2021-05-22 19:09:56 -0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -44,9 +44,8 @@ module.exports = {
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
+  pageUrlOverrides: {
+    '/fsd-tesla-vision-teleops': '44db4144527a440c84b44e38790e05ab'
+  }
   pageUrlOverrides: null
 }
